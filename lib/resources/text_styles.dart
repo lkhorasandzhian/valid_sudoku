83bd681148d7a83ba0cb9456465181a5fc6081a6
lkhorasandzhian <levon.kh228@gmail.com>
2023-04-04 22:12:39 +0300

    text styles for title and button were added

---
+++ lib/resources/text_styles.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static TextStyle titleTextStyle = const TextStyle(
+    fontSize: 50,
+    color: Colors.black,
+  );
+
+  static TextStyle buttonTextStyle = const TextStyle(
+    fontSize: 18,
+    color: Colors.black,
+  );
+}
